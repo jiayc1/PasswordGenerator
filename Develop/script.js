924cd6444c9df533e441a6a3f5e561ee517987a7
Jess <jess.chen0502@gmail.com>
2020-12-21 17:10:55 -0800

    final hw

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,131 @@
+// Password Generator 
+
+// Special Characters Array 
+var specialCharacters = [
+  '@','%','+','\\','/',"'",'!','#','$','^','?',
+  ':',',',')','(','}','{',']','[','~','-','_',
+  '.'
+];
+
+// Numeric characters Array 
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Lowercase characters Array 
+var lowerCasedCharacters = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+// Uppercase Characetrs Array 
+var upperCasedCharacters = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+var writePassword = function() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// Get Password Options
+function generatePassword() {
 
-function getPasswordOptions () {
+  var passwordOptions = getPasswordOptions();
+
+  var results = [];
+  var guaranteeChars = [];
+  var forSureChars = [];
+
+  if(passwordOptions.hasSpecialCharacters === true) {
+
+    forSureChars = forSureChars.concat(specialCharacters);
+    guaranteeChars.push(specialCharacters[getRandom(specialCharacters.length)]);
+  }
+
+  if(passwordOptions.hasNumericCharacters === true) {
+    forSureChars = forSureChars.concat(numericCharacters);
+    guaranteeChars.push(numericCharacters[getRandom(numericCharacters.length)]);
+  }
+  if(passwordOptions.hasLowerCasedCharacters === true) {
+    forSureChars = forSureChars.concat(lowerCasedCharacters);
+    guaranteeChars.push(lowerCasedCharacters[getRandom(lowerCasedCharacters.length)]);
+  }
+  if(passwordOptions.hasUpperCasedCharacters === true) {
+    forSureChars = forSureChars.concat(upperCasedCharacters);
+    guaranteeChars.push(upperCasedCharacters[getRandom(upperCasedCharacters.length)]);
+  }
+  
+  for(var i = 0; i < passwordOptions.length; i++) {
+
+    results.push(forSureChars[getRandom(forSureChars.length)]);
+
+  }
+
+  for(var j = 0; j < guaranteeChars.length; j++) {
+    results[j] = guaranteeChars[j];
+  }
+
+  return results.join('');
+  
+}
+  
+//Get Password Options
+
+ function getPasswordOptions () {
   var passwordLength = prompt("Password Length"); // Prompts user for passsword length 
   var passwordLengthInInt = parseInt(passwordLength, 10); // converts object to string Int 
-}
+  
   if(isNaN(passwordLengthInInt) === true) { // if the variable is not a number, return this alert to let them know that it needs to be a number 
   alert("Please provide a number for the length of the password");
   return;
   }
+
+
+  if(passwordLengthInInt < 8) { // This sets a minimum of at least 8 to the password length
+  alert("Please enter a number to at least 8")
+  return; 
+  }
+
+
+  if(passwordLengthInInt > 128) { // set the max length to 128 
+  alert("The max is 128, please don't go over 128")
+  return;
+  }
+
+  var isAllowLowercase = confirm("Allow Lowercases?");
+  var isAllowUpper = confirm("Allow Uppercases?");
+  var isAllowNumbers = confirm("Allow Numbers?");
+  var isAllowSpecialChar = confirm("Allow Characters?");
+
+  if(isAllowLowercase === false && isAllowUpper === false &&
+    isAllowNumbers === false && isAllowSpecialChar === false) {
+
+      alert("You will need to select one");
+      return; 
+  }
+  // storing smaller variables into a big variable
+  var passwordOptions = {
+    length: passwordLengthInInt,
+    hasSpecialCharacters: isAllowSpecialChar,
+    hasNumericCharacters: isAllowNumbers,
+    hasLowerCasedCharacters: isAllowLowercase,
+    hasUpperCasedCharacters: isAllowUpper
+  };
+
+  return passwordOptions;
+}
+
+
+function getRandom(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+  
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # PasswordGenerator
+## Acceptance Criterias 
+
+1) GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+
+In order to be able to generate a password when I am presented with a series of prompts for password criteria - I will first have to create prompts function that will allow me to have a pop up with OK and Cancel button and a textbox. 
+
+
+
+2) WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+
+The criteria added is that the length must be in number form else it will give the user an alert to enter in a number for the length of the password. 
+
+
+
+
+3)WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+Added two IF statements to set the minimum to at least 8 and no more than 128 characters for the user. 
+
+
+4)WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+This prompts to show or ask the user if they want lowercase, uppercase, numeric, and/or special characters. In order to write this I will use the confirm statement. 
+
+
+5) WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+In order to do so - I have added false statements into the code to make sure that at least one character type has to be selected else it would not work. 
+
+
+6) WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+Create a function to generate password when all selected criterias has been met. 
+
+7)WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+When I click a button, it called writepassword function which calls generating password. 
+
+
+
